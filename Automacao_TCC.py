17210a5cd1814e97bc9439611c40b5f587fbe1c7
Woudson <91691436+Woudson@users.noreply.github.com>
2024-10-22 19:51:03 -0300

    Atualizações no Codigo, Translate

--- Automacao_TCC.py
+++ Automacao_TCC.py
@@ -2,3 +2,19 @@
 # Interface simples com 3 seções
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
 
+from googletrans import Translator
+
+# Criar uma instância do tradutor
+translator = Translator()
+
+# Texto a ser traduzido
+text = input("Digite o texto que você deseja traduzir: ")
+
+# Escolher o idioma de destino (por exemplo, inglês - 'en')
+target_language = input("Digite o código do idioma de destino (por exemplo, 'en' para inglês): ")
+
+# Fazer a tradução
+translation = translator.translate(text, dest=target_language)
+
+# Exibir a tradução
+print(f"Texto traduzido: {translation.text}")
